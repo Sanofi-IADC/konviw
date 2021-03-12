e09237c8da5389cd6812ffe3da36c001aec6aa5c
PiiXiieeS <piixiiees@gmail.com>
2021-03-13 00:41:32 +0100

    build: deploy via gh-pages npm package

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,14 @@ module.exports = {
   description: 'Enterprise public viewer for your Confluence pages.',
   base: '/konviw/', // when published to GitHub Pages
   // base: '/', // when rendered locally
+  head: [['link', { rel: 'icon', href: '/konviw.png' }]],
   themeConfig: {
     logo: '/konviw.svg',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Introduction', link: '/introduction' },
       { text: 'Installation', link: '/installation' },
+      { text: 'Usage', link: '/usage' },
       { text: 'About', link: '/about' },
       { text: 'GitHub', link: 'https://github.com/Sanofi-IADC/konviw' },
     ],
---
+++ docs/usage.md
@@ -0,0 +1,12 @@
+---
+title: Usage
+---
+
+# Getting started
+
+After following the [installation](/installation) and configuration steps you can render any Confluence page composing
+`CPV_BASEHOST:PORT + CPV_BASEPATH + /wiki/spaces/ + spaceKey + /pages/ + pageId`
+
+## Emojis
+
+Konviw use standard UTF8 emojis
