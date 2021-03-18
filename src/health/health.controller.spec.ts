e38971b2e4823a01f06c0e78822bf103b775a93e
MeStrak <call_me_nigel@yahoo.co.uk>
2021-03-18 22:55:22 +0000

    test: add health check unit test

---
+++ src/health/health.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/common';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
+
+import { HealthController } from './health.controller';
+
+describe('HealthController', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+      imports: [HttpModule],
+      providers: [HealthCheckService, HttpHealthIndicator, HealthCheckExecutor],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
