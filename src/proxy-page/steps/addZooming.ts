365fe4573b59bfb73e67fe8e089e70c0719da0da
PiiXiieeS <piixiiees@gmail.com>
2021-03-25 03:33:46 +0100

    perf(script): load scripts in html body
    Move the load of javascript modules at the end of the body instead of
    at the end of the #Content div tag

--- src/proxy-page/steps/addZooming.ts
+++ src/proxy-page/steps/addZooming.ts
@@ -11,18 +11,15 @@ export default (config: ConfigService): Step => {
 
     // Library to include the zooming effect to Drawio images
     // https://unpkg.com/zooming@2.1.1/build/zooming.min.js
-    $('head').append(
-      // TODO: In the future move from the CDN path into a local bundle built via webpack
-      `<script src="/zooming/zooming-2.1.1.min.js?cache=${version}"></script>`,
-    );
-
     // When the DOM content is loaded call the initialization of the Zooming library
-    $('#Content').append(
-      `<script>
+    $('body').append(
+      `<script defer src="/zooming/zooming-2.1.1.min.js?cache=${version}"></script>
+      <script type="module">
         document.addEventListener('DOMContentLoaded', function () {
           new Zooming({}).listen('.img-zoomable');
-          new Zooming({}).listen('.confluence-embedded-image')})
-          </script>`,
+          new Zooming({}).listen('.confluence-embedded-image')
+        })
+      </script>`,
     );
     context.getPerfMeasure('addZooming');
   };
