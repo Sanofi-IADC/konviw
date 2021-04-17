ad351f8dd14260314d71c536e8228b6ec598cad2
PiiXiieeS <piixiiees@gmail.com>
2021-04-18 01:20:24 +0200

    fix(sass): add basePath to prefix sassMiddleware

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import path, { join } from 'path';
+import path from 'path';
 import { AppModule } from './app.module';
 import sassMiddleware from 'node-sass-middleware';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -15,7 +15,6 @@ async function bootstrap() {
   // logger: ['error', 'warn'];
   const config = app.get(ConfigService);
   const basePath = config.get<Config>('web.basePath');
-  const nodeEnv = config.get<Config>('env');
   app.useGlobalPipes(
     // Reference: https://docs.nestjs.com/techniques/validation#auto-validation
     new ValidationPipe({
@@ -35,29 +34,20 @@ async function bootstrap() {
   app.setGlobalPrefix(`${basePath}`);
   app.disable('x-powered-by');
   app.enableCors();
+
   app.use(
     sassMiddleware({
       src: path.resolve('./src/assets/scss'),
-      dest:
-        `${nodeEnv}` === 'production'
-          ? join(__dirname, '..', 'static')
-          : path.resolve('./static/css'),
+      dest: path.resolve('./static/css'),
       debug: true,
       outputStyle: 'compressed',
       log: function (severity: string, key: string, value: string) {
         logger.log(`${key} : ${value}`, `node-sass-middleware : ${severity}`);
       },
-      prefix: '/css',
+      prefix: `${basePath}/css`,
     }),
   );
-  app.useStaticAssets(
-    `${nodeEnv}` === 'production'
-      ? join(__dirname, '..', 'static')
-      : path.resolve('./static'),
-    {
-      prefix: `${basePath}`,
-    },
-  );
+  app.useStaticAssets(path.resolve('./static'), { prefix: `${basePath}` });
 
   await app.listen(process.env.PORT || 3000);
 }
