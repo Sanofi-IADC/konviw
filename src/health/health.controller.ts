108bd5634624803cb733069df311744d08c2bb50
Henri Valette <henri.valette.fr@gmail.com>
2021-04-20 10:27:50 +0200

    fix(health): remove atlassian api call (#93)

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,36 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
-import {
-  HealthCheck,
-  HealthCheckService,
-  HttpHealthIndicator,
-} from '@nestjs/terminus';
-import { ConfigService } from '@nestjs/config';
-import Config from '../config/config.d';
+import { HealthCheck } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private config: ConfigService,
-    private health: HealthCheckService,
-    private dns: HttpHealthIndicator,
-  ) {}
-
   @Get()
   @HealthCheck()
   check() {
-    const baseURL = this.config.get<Config>('confluence.baseURL');
-    const proxyUrl = (this.config.get<Config>(
-      'httpsProxy',
-    ) as unknown) as string;
-    let options = {};
-    if (proxyUrl) {
-      const host = proxyUrl.slice(0, proxyUrl.length - 5);
-      const port = proxyUrl.slice(-4);
-      options = { proxy: { host, port } };
-    }
-    return this.health.check([
-      async () =>
-        this.dns.pingCheck('Atlassian API', `${baseURL}`, { ...options }),
-    ]);
+    // return this.health.check([
+    //   async () => this.dns.pingCheck('Atlassian API', `${baseURL}`),
+    // ]);
+
+    return {
+      status: 'ok',
+      info: { Konviw: { status: 'up' } },
+      error: {},
+      details: { Konviw: { status: 'up' } },
+    };
   }
 }
