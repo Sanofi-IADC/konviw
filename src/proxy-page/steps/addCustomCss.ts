4a1a68b2adbb2f21f15df5635e5bf46b317d8d25
Henri Valette <henri.valette.fr@gmail.com>
2021-04-22 10:08:05 +0200

    feat(theme): add theme style param to change theme

--- src/proxy-page/steps/addCustomCss.ts
+++ src/proxy-page/steps/addCustomCss.ts
@@ -2,16 +2,26 @@ import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 import { ConfigService } from '@nestjs/config';
 import Config from '../../config/config';
+import fs from 'fs';
+import { join, dirname } from 'path';
 
-export default (config: ConfigService): Step => {
+export default (config: ConfigService, style?: string): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('addCustomCss');
     const $ = context.getCheerioBody();
     const version = config.get<Config>('version');
     const basePath = config.get<Config>('web.basePath');
 
+    let cssPath = `${basePath}/css/custom.css?cache=${version}`;
+    const path = `${dirname(
+      require.main.filename,
+    )}/../static/css/${style}/custom.css`;
+    if (fs.existsSync(path)) {
+      cssPath = `${basePath}/css/${style}/custom.css?cache=${version}`;
+    }
+
     $('head').append(
-      `<link rel="stylesheet" type="text/css" href="${basePath}/css/custom.css?cache=${version}" />`,
+      `<link rel="stylesheet" type="text/css" href="${cssPath}" />`,
     );
 
     // ! Do not insert internal CSS styles because the function removeUnnecessaryCode will remove them later
