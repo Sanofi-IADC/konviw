fd4d69878801aaf2814605b6255ff59236e30a2b
PiiXiieeS <piixiiees@gmail.com>
2021-04-24 09:24:35 +0200

    fix(health): healthcheck with Atlassian API check

---
+++ src/health/health-atlassian.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfluenceService } from '../confluence/confluence.service';
+import {
+  HealthIndicator,
+  HealthIndicatorResult,
+  HealthCheckError,
+} from '@nestjs/terminus';
+
+@Injectable()
+export class ApiHealthService extends HealthIndicator {
+  private readonly logger = new Logger(ApiHealthService.name);
+  constructor(private confluence: ConfluenceService) {
+    super();
+  }
+
+  /**
+   * @function apiCheck Service
+   * @description Call the API service from Atlassian to check it is alive
+   * @return Promise {HealthIndicatorResult}
+   */
+  async apiCheck(): Promise<HealthIndicatorResult> {
+    let isHealthy = false;
+    try {
+      isHealthy = await this.confluence.getAllPosts('konviw');
+    } catch (error) {
+      isHealthy = false;
+    }
+    let result;
+    if (isHealthy) {
+      result = this.getStatus('Atlassian API', isHealthy, { message: 'up' });
+      this.logger.log(`Health Status is Up: ${JSON.stringify(result)}`);
+      return result;
+    } else {
+      result = this.getStatus('Atlassian API', isHealthy, { message: 'down' });
+      this.logger.log(`Health Status is Down: ${JSON.stringify(result)}`);
+    }
+    throw new HealthCheckError('Atlassian API failed', result);
+  }
+}
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,20 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { ApiHealthService } from './health-atlassian.service';
 
 @Controller('health')
 export class HealthController {
+  constructor(
+    private readonly apiHealth: ApiHealthService,
+    private readonly health: HealthCheckService,
+  ) {}
+
+  /**
+   * @GET (controller)
+   * @description Health check controller to show Konviw and Atlassian API status
+   * @return {JSON} '{"status": "ok"}' - terminus JSON response
+   */
   @Get()
   @HealthCheck()
-  check() {
-    // return this.health.check([
-    //   async () => this.dns.pingCheck('Atlassian API', `${baseURL}`),
-    // ]);
-
-    return {
-      status: 'ok',
-      info: { Konviw: { status: 'up' } },
-      error: {},
-      details: { Konviw: { status: 'up' } },
-    };
+  apiCheck() {
+    return this.health.check([async () => this.apiHealth.apiCheck()]);
   }
 }
