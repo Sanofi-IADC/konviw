a4ae873873c34e03e65851793ae53167038fafab
PiiXiieeS <piixiiees@gmail.com>
2021-05-02 18:53:37 +0200

    Breaking: reaching first stable release

--- src/proxy-page/steps/addHighlightjs.ts
+++ src/proxy-page/steps/addHighlightjs.ts
@@ -1,14 +1,13 @@
 import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 import { ConfigService } from '@nestjs/config';
-import Config from '../../config/config';
 
 export default (config: ConfigService): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('addHighlightjs');
     const $ = context.getCheerioBody();
-    const version = config.get<Config>('version');
-    const basePath = config.get<Config>('web.basePath');
+    const version = config.get('version');
+    const basePath = config.get('web.basePath');
 
     $('pre.syntaxhighlighter-pre').each(
       (_index: number, macro: CheerioElement) => {
@@ -18,13 +17,14 @@ export default (config: ConfigService): Step => {
 
     // `<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.2/styles/default.min.css" />`
     $('head').append(
-      `<link rel="stylesheet" type="text/css" href="${basePath}/highlight/zenburn.min.css?nocache=${version}" />`,
+      // `<link rel="stylesheet" type="text/css" href="${basePath}/highlight/zenburn.min.css?cache=${version}" />`,
+      `<link href="${basePath}/highlight/zenburn.min.css?cache=${version}" rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" />`,
     );
 
     // `<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.2/highlight.min.js"></script>`
     // When the DOM content is loaded call the initialization of the Hightlight library
     $('body').append(
-      `<script defer src="${basePath}/highlight/highlight.min.js?nocache=${version}"></script>
+      `<script defer src="${basePath}/highlight/highlight.min.js?cache=${version}"></script>
        <script type="module">
          document.addEventListener('DOMContentLoaded', function () {hljs.initHighlightingOnLoad();})
        </script>`,
--- src/proxy-page/steps/fixHtmlHead.ts
+++ src/proxy-page/steps/fixHtmlHead.ts
@@ -1,5 +1,4 @@
 import { ConfigService } from '@nestjs/config';
-import Config from 'src/config/config';
 import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 
@@ -7,17 +6,17 @@ export default (config: ConfigService): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('fixHtmlHead');
     const $ = context.getCheerioBody();
-    const basePath = config.get<Config>('web.basePath');
+    const basePath = config.get('web.basePath');
 
     $('head').prepend(
-      `<link rel="apple-touch-icon" sizes="180x180" href="${basePath}/favicon/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="${basePath}/favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="${basePath}/favicon/favicon-16x16.png">
-      <link rel="shortcut icon" href="${basePath}/favicon/favicon.ico">`,
+      `<link rel="apple-touch-icon" sizes="180x180" href="${basePath}/favicon/apple-touch-icon.png">`,
+      `<link rel="icon" type="image/png" sizes="32x32" href="${basePath}/favicon/favicon-32x32.png">`,
+      `<link rel="icon" type="image/png" sizes="16x16" href="${basePath}/favicon/favicon-16x16.png">`,
+      `<link rel="shortcut icon" href="${basePath}/favicon/favicon.ico">`,
     );
 
     $('head').prepend(
-      '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">',
+      `<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">`,
     );
 
     context.getPerfMeasure('fixHtmlHead');
