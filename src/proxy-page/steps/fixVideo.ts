51827526cdae81f60092243c4583150b863b5e09
PiiXiieeS <piixiiees@gmail.com>
2021-05-16 02:45:09 +0200

    feat(video): add poster with preview for the video
    Also improve performance because in the earlier version
    the function was checking all the image wrappers

--- src/proxy-page/steps/fixVideo.ts
+++ src/proxy-page/steps/fixVideo.ts
@@ -1,25 +1,51 @@
 import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 
+/**
+ * ### Proxy page step to add Video tag to display mp4 video attachments.
+ *
+ * This module gets Cheerio to search 'span.confluence-embedded-file-wrapper'
+ * get the 'href' URL and append a 'video' html5 tag.
+ * This step assumes an image with extension .webp and same name as the video can be
+ * used to previsualize a picture when the video is not running.
+ *
+ * @param  {ConfigService} config
+ * @returns void
+ */
 export default (): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('fixVideo');
     const $ = context.getCheerioBody();
 
     // Div class with span.confluence-embedded-file-wrapper is used for images and videos
-    $('span.confluence-embedded-file-wrapper').each(
+    // while only the videos come with an 'a' pointing to the video attachment.
+    // This narrow filter improves the performance as now the function only goes thru the videos.
+    $('span.confluence-embedded-file-wrapper a').each(
       (_index: number, fileWrapper: CheerioElement) => {
-        const thisBlock = $(fileWrapper).html();
-        if (
-          // And we search for the attachments which are video format
-          thisBlock &&
-          thisBlock.match(/href([^|]*)(>.*).(mp4|avi|mov|flv|wmv)/g)
-        ) {
-          $(fileWrapper).prepend(
-            `<video width="400" controls><source src="${$(fileWrapper)
-              .children('a')
-              .attr('href')}" type="video/mp4"></video>`,
-          );
+        const searchMedia = new RegExp(`(\/.*)\/(.*).(mp4|avi|mov|flv|wmv)`);
+        // Search for the path $1, title $2 and extension $3 (not used)
+        const [, pathMedia, titleMedia, extMedia] =
+          searchMedia.exec($(fileWrapper).attr('href')) ?? [];
+        if (pathMedia) {
+          // Append the video tag with src to the attachment and an image as poster
+          $(fileWrapper)
+            .parent()
+            .append(
+              `<video poster="${pathMedia}/${titleMedia}.webp" controls>
+                <source src="${$(fileWrapper).attr(
+                  'href',
+                )}" type="video/mp4"></video>`,
+            );
+
+          // Append the name of the file as caption for the video
+          $(fileWrapper)
+            .parent()
+            .append(
+              `<span class="smalltext">${decodeURI(titleMedia)}.${extMedia}
+            </span>`,
+            );
+
+          $(fileWrapper).remove();
         }
       },
     );
