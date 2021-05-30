f1d0a4f4a725f8047b68fb8e0cd121a21fcf89d9
PiiXiieeS <piixiiees@gmail.com>
2021-05-30 03:20:16 +0200

    fix(fixEmojis): display img with Atlassian emojis

--- src/proxy-page/steps/fixEmojis.ts
+++ src/proxy-page/steps/fixEmojis.ts
@@ -1,23 +1,27 @@
 import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
+import { ConfigService } from '@nestjs/config';
 
-export default (): Step => {
+export default (config: ConfigService): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('fixEmojis');
     const $ = context.getCheerioBody();
+    const confluenceBaseUrl = config.get('confluence.baseURL');
 
     // img tag with class emoticon is used to wrap the Confluence emoticons
     $('img.emoticon').each((_index: number, element: CheerioElement) => {
       const thisEmoji = $(element).data();
-      if (
-        thisEmoji.emojiId.substring(0, 9) === 'atlassian' || // 1st condition to detect special Atlassian emoticons
-        thisEmoji.emojiId.substring(5, 6) === '-' // 2nd condition to detect country flags which are not yet supported in Win10
-      ) {
-        // TODO: Replace this fallback for an image retrived from CDN of special Atlassian emoticons
-        // For instance https://pf-emoji-service--cdn.us-east-1.prod.public.atl-paas.net/atlassian/check_mark_64.png for :check_mark:
-        $(element).replaceWith(thisEmoji.emojiFallback);
+      // condition to detect special Atlassian emoticons
+      if (thisEmoji.emojiId.substring(0, 9) === 'atlassian') {
+        const fullUrl = $(element)
+          .attr('src')
+          .replace(/\/cpv/, confluenceBaseUrl);
+        $(element).attr('src', fullUrl);
+        $(element).attr('srcset', fullUrl);
+        console.log(fullUrl);
       } else {
-        $(element).replaceWith(`&#x${thisEmoji.emojiId}`);
+        // go for the fallback emoticon provided by the API
+        $(element).replaceWith(thisEmoji.emojiFallback);
       }
     });
     context.getPerfMeasure('fixEmojis');
