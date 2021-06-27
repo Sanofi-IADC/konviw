78aba86ea071d7d517ddd7f5249cb9064feca597
PiiXiieeS <piixiiees@gmail.com>
2021-06-27 12:39:26 +0200

    feat(addJira): improvements jira macro integration
    - management of JIRA multi-instance
    - positioning of the Jira table in the right position in the page
    - adding title with the filter for the table

--- src/proxy-page/steps/addJira.ts
+++ src/proxy-page/steps/addJira.ts
@@ -19,10 +19,19 @@ export default (config: ConfigService): Step => {
       return;
     }
 
+    const elementTags = [];
+    // this is the outer div used to wrap the Jira issues macro
+    // which it is saved to place the tables just before
+    $('.confluence-jim-macro').each((_, elementJira: cheerio.TagElement) => {
+      elementTags.push(elementJira);
+    });
+
     const jiraIssuesPromises = [];
+    // this is the div holding the data to scrap the list of issues
     $('.refresh-wiki').each((_, elementJira: cheerio.TagElement) => {
       const wikimarkup: string = elementJira.attribs['data-wikimarkup'];
       const xmlWikimarkup = cheerio.load(wikimarkup, { xmlMode: true });
+      const server = xmlWikimarkup('ac\\:parameter[ac\\:name="server"]').text();
       const filter = xmlWikimarkup(
         'ac\\:parameter[ac\\:name="jqlQuery"]',
       ).text();
@@ -36,148 +45,158 @@ export default (config: ConfigService): Step => {
       jiraIssuesPromises.push({
         issues: {
           issues: jiraService
-            .findTickets(filter, columns, Number(maximumIssues))
+            .findTickets(server, filter, columns, Number(maximumIssues))
             .then((res) => res.issues),
         },
         columns,
+        server,
+        filter,
       });
     });
 
+    // add the grid using http://gridjs.io library
+    $('head').append(
+      `<link href="${basePath}/gridjs/mermaid.min.css?cache=${version}" rel="stylesheet" />`,
+    );
+    $('body').append(
+      `<script defer src="${basePath}/gridjs/gridjs.production.min.js?cache=${version}"></script>`,
+    );
+
+    // TODO: to handle a potential failure of one of the promises gracefully
     const jirasIssues = await Promise.all(
       jiraIssuesPromises.map((j) => j.issues.issues),
     );
+
     const issuesColumns = jiraIssuesPromises.map((jira, i) => ({
       columns: jira.columns,
       issues: jirasIssues[i],
+      element: elementTags[i],
+      server: jira.server,
+      filter: jira.filter,
     }));
 
-    issuesColumns.forEach(({ issues, columns }, index) => {
-      const data = [];
-      issues.forEach((issue) => {
-        data.push({
-          key: {
-            name: issue.key,
-            link: `${config.get('confluence.baseURL')}/browse/${
-              issue.key
-            }?src=confmacro`,
-          },
-          t: {
-            name: issue.fields.issuetype.name,
-            icon: issue.fields.issuetype?.iconUrl,
-          },
-          summary: {
-            name: issue.fields.summary,
-            link: `${config.get('confluence.baseURL')}/browse/${
-              issue.key
-            }?src=confmacro`,
-          },
-          updated: issue.fields.updated
-            ? `${new Date(issue.fields.updated).toLocaleString('en-EN', {
-                year: 'numeric',
-                month: 'short',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-              })}`
-            : '',
-          assignee: issue.fields.assignee?.displayName,
-          pr: {
-            name: issue.fields.priority?.name,
-            icon: issue.fields.priority?.iconUrl,
-          },
-          status: {
-            name: issue.fields.status?.name,
-            color: issue.fields.status?.statusCategory.colorName,
-          },
-          resolution: issue.fields.resolution?.name,
+    issuesColumns.forEach(
+      ({ issues, columns, element, server, filter }, index) => {
+        const data = [];
+        // Load new base URL if defined a specific connection for Jira as ENV variables
+        let baseUrl =
+          process.env[`CPV_JIRA_${server.replace(/\s/, '_')}_BASE_URL`];
+        if (!baseUrl) baseUrl = config.get('confluence.baseURL');
+        issues.forEach((issue) => {
+          data.push({
+            key: {
+              name: issue.key,
+              link: `${baseUrl}/browse/${issue.key}?src=confmacro`,
+            },
+            t: {
+              name: issue.fields.issuetype.name,
+              icon: issue.fields.issuetype?.iconUrl,
+            },
+            summary: {
+              name: issue.fields.summary,
+              link: `${baseUrl}/browse/${issue.key}?src=confmacro`,
+            },
+            updated: issue.fields.updated
+              ? `${new Date(issue.fields.updated).toLocaleString('en-EN', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}`
+              : '',
+            assignee: issue.fields.assignee?.displayName,
+            pr: {
+              name: issue.fields.priority?.name,
+              icon: issue.fields.priority?.iconUrl,
+            },
+            status: {
+              name: issue.fields.status?.name,
+              color: issue.fields.status?.statusCategory.colorName,
+            },
+            resolution: issue.fields.resolution?.name,
+          });
         });
-      });
 
-      const requestedFields = columns.split(',');
+        const requestedFields = columns.split(',');
 
-      let gridjsColumns = `[{
+        let gridjsColumns = `[{
                 name: 'Key',
                 sort: {
                   compare: (a, b) => (a.name > b.name ? 1 : -1),
                 },
                 formatter: (cell) => gridjs.html(${'`<a href="${cell.link}" target="_blank">${cell.name}</a>`'})
               },`;
-      if (requestedFields.includes('summary')) {
-        gridjsColumns += `{
+        if (requestedFields.includes('summary')) {
+          gridjsColumns += `{
                 name: 'Summary',
                 sort: {
                   compare: (a, b) => (a.name > b.name ? 1 : -1),
                 },
                 formatter: (cell) => gridjs.html(${'`<a href="${cell.link}" target="_blank">${cell.name}</a>`'})
               },`;
-      }
+        }
 
-      if (requestedFields.includes('issuetype')) {
-        gridjsColumns += `{
+        if (requestedFields.includes('issuetype')) {
+          gridjsColumns += `{
                 name: 'T',
                 sort: {
                   compare: (a, b) => (a.name > b.name ? 1 : -1),
                 },
                 formatter: (cell) => gridjs.html(cell ? ${'`<img src="${cell.icon}" style="height:25px;padding:0"/>`'} : ''),
               },`;
-      }
-      if (requestedFields.includes('status')) {
-        gridjsColumns += `{
+        }
+        if (requestedFields.includes('status')) {
+          gridjsColumns += `{
                 name: 'Status',
                 sort: {
                   compare: (a, b) => (a.name > b.name ? 1 : -1),
                 },
                 formatter: (cell) => gridjs.html(${'`<div style="color:${cell.color}">${cell.name}</div>`'})
               },`;
-      }
-      if (requestedFields.includes('updated')) {
-        gridjsColumns += `{
+        }
+        if (requestedFields.includes('updated')) {
+          gridjsColumns += `{
                 name: 'Updated',
                 sort: {
                   compare: (a, b) => (new Date(a) > new Date(b) ? 1 : -1),
                 }
               },`;
-      }
-      if (requestedFields.includes('assignee')) {
-        gridjsColumns += `{
+        }
+        if (requestedFields.includes('assignee')) {
+          gridjsColumns += `{
                 name: 'Assignee',
               },`;
-      }
-      if (requestedFields.includes('priority')) {
-        gridjsColumns += `{
+        }
+        if (requestedFields.includes('priority')) {
+          gridjsColumns += `{
                 name: 'Pr',
                 sort: {
                   compare: (a, b) => (a.name > b.name ? 1 : -1),
                 },
                 formatter: (cell) => gridjs.html(cell ? ${'`<img src="${cell.icon}" style="height:25px;padding:0"/>`'} : ''),
               },`;
-      }
-      if (requestedFields.includes('resolution')) {
-        gridjsColumns += ` {
+        }
+        if (requestedFields.includes('resolution')) {
+          gridjsColumns += ` {
                 name: 'Resolution',
               },`;
-      }
-      gridjsColumns += ']';
+        }
+        gridjsColumns += ']';
 
-      // remove the header
-      $('div[id^="jira-issues-"]').remove();
+        // remove the header
+        $('div[id^="jira-issues-"]').remove();
 
-      // remove the 'loading...' text
-      $('div[id^="refresh-issues-loading-"]').remove();
+        // remove the 'loading...' text
+        $('div[id^="refresh-issues-loading-"]').remove();
 
-      // remove the actualize link
-      $('.refresh-issues-bottom').remove();
+        // remove the actualize link
+        $('.refresh-issues-bottom').remove();
 
-      // add the grid using http://gridjs.io library
-
-      $('head').append(
-        `<link href="${basePath}/gridjs/mermaid.min.css?cache=${version}" rel="stylesheet" />`,
-      );
-
-      $('body').append(
-        `<script defer src="${basePath}/gridjs/gridjs.production.min.js?cache=${version}"></script>`,
-        `<div id="gridjs${index}"></div>`,
-        `<script>
+        $(element).before(
+          `<strong>Jira issues for ${filter}</strong>`,
+          `<div id="gridjs${index}"></div>`,
+          `<script>
         document.addEventListener('DOMContentLoaded', function () {
           new gridjs.Grid({
             columns: ${gridjsColumns},
@@ -199,8 +218,10 @@ export default (config: ConfigService): Step => {
           }).render(document.getElementById("gridjs${index}"));
         })
       </script>`,
-      );
-    });
+        );
+        $(element).remove();
+      },
+    );
     context.getPerfMeasure('addJira');
   };
 };
