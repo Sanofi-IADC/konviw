59ff3ddcec260fb49af8cf723c56ae49e463dc25
piixiiees <piixiiees@gmail.com>
2021-07-04 02:22:30 +0200

    feat(404): friendly 404 message

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(config));
 
   app.setGlobalPrefix(`${basePath}`);
   app.disable('x-powered-by');
