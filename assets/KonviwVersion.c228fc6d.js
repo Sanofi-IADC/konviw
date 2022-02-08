624a3ba6aef4c8b4ce670eb921c1e0ee2cc09f81
kiru42 <kiru42@users.noreply.github.com>
2022-02-08 13:26:13 +0000

    deploy: 0bd27eccdc87c466c78e3f8bd65358f517ea063c

--- assets/KonviwVersion.c228fc6d.js
+++
@@ -1,1 +0,0 @@
-import{t as o}from"./app.8053bb3c.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const e="2.6.0",s={name:"KonviwVersion",data(){return{konviwVersion:e}}};function t(i,p,a,c,r,_){return o(r.konviwVersion)}var v=n(s,[["render",t]]);export{v as default};
--- assets/demoCharts.html.f106a3c1.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as t,e as a}from"./app.8053bb3c.js";import{_ as c}from"./plugin-vue_export-helper.21dcd24c.js";const r={};function n(s,p){const e=o("ConfluencePage");return t(),a(e,{metadata:!1,switchTheme:!1,type:"title",pageId:"44007425"})}var f=c(r,[["render",n]]);export{f as default};
