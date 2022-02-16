725dc87bb47e5da2e9055488eeccd8ee96df2e7d
Kiruban Premkumar <kiruban.premkumar-ext@sanofi.com>
2022-02-16 22:15:59 +0100

    fix: FND-593 cypress type definition conflit with jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testMatch: ['**/tests/unit/**/*.spec.(ts|js)|**/__tests__/*.(ts|js)'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/dist/', 'node_modules', '/docs/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/src/**/*.{js,ts}',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!**/cache/**',
+    '!**/coverage/**',
+    '!**/src/static/**',
+  ],
+  coverageDirectory: 'tests/unit/coverage',
+  testEnvironment: 'node',
+};
