94c916e19426260a47b0d5ff6c0bdee8db7b2f2a
PiiXiieeS <PiiXiieeS@users.noreply.github.com>
2022-02-28 19:46:34 +0000

    deploy: 515605faae38f718cd96117e6778a66069925e7a

---
+++ assets/demoIntroduction.html.1731db33.js
@@ -0,0 +1,1 @@
+const t={key:"v-f3f1bffa",path:"/demoIntroduction.html",title:"Demo with Title",lang:"en-US",frontmatter:{title:"Demo with Title"},excerpt:"",headers:[],git:{updatedTime:1646077547e3,contributors:[{name:"Jose Gascon",email:"PiiXiieeS@users.noreply.github.com",commits:1}]},filePathRelative:"demoIntroduction.md"};export{t as data};
