c07e1601e420389e28845d5cffb0e9602d5e1af0
kiru42 <kiru42@users.noreply.github.com>
2022-03-09 12:13:40 +0000

    deploy: fe1b385f5f8a40f728c518d9e74dbf12021aecb7

--- assets/demoJira.html.27a20246.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as t,g as a}from"./app.f1c36306.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const r={};function c(s,_){const e=o("ConfluencePage");return t(),a(e,{metadata:!1,switchTheme:!1,type:"title",pageId:"28278785"})}var l=n(r,[["render",c]]);export{l as default};
