18b97fbbde47d61ac2dc1c2f9563f47f2f55870e
kiru42 <kiru42@users.noreply.github.com>
2022-03-14 16:08:02 +0000

    deploy: 65de05b845fe43ebff23207680176e83f3eececc

---
+++ assets/KonviwVersion.5fab30ba.js
@@ -0,0 +1,1 @@
+import{t as r}from"./app.efb4e02e.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const e="2.10.20",s={name:"KonviwVersion",data(){return{konviwVersion:e}}};function t(i,a,c,p,o,_){return r(o.konviwVersion)}var v=n(s,[["render",t]]);export{v as default};
