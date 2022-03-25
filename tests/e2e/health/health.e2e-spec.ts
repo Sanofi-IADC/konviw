9951566393e1b66d50d008cc2cee435c1f27e405
Kiruban Premkumar <kiruban.premkumar-ext@sanofi.com>
2022-03-25 10:45:51 +0100

    fix: health check

--- tests/e2e/health/health.e2e-spec.ts
+++ tests/e2e/health/health.e2e-spec.ts
@@ -1,8 +1,25 @@
+import { NestFactory } from '@nestjs/core';
 import request from 'supertest';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
 
 describe('HealthController (e2e)', () => {
+
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    app = await NestFactory.create(AppModule, {
+      logger: false,
+    });
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('Returns healthy status', async () => {
-    const res = await request(global.app.getHttpServer()).get('/health');
-    expect(res['res'].statusCode).toBe(200);
+    const res = await request(app.getHttpServer()).get('/health');
+    expect(res.statusCode).toBe(HttpStatus.OK);
   });
 });
