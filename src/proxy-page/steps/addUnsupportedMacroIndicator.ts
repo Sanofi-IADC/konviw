c8a7afe7952358b187a30001fba78a67dde4303c
PiiXiieeS <piixiiees@gmail.com>
2022-04-02 17:09:19 +0200

    fix: debug also supported macros
    - new class to debug supported macros
    - debug macro TOC
    - highligh unsupported macro page-tree-search
    - move unsupported-macros css to debug.scss

--- src/proxy-page/steps/addUnsupportedMacroIndicator.ts
+++ src/proxy-page/steps/addUnsupportedMacroIndicator.ts
@@ -5,41 +5,32 @@ import * as cheerio from 'cheerio';
 /**
  * ### Proxy page step to notify the unprocessed/unsupported macros in the page
  *
- *
  * @param  {ConfigService} config
  * @returns void
  */
- export default (): Step => {
+export default (): Step => {
   return (context: ContextService): void => {
     context.setPerfMark('addUnsupportedMacroIndicator');
     const $ = context.getCheerioBody();
 
-    const macrosFound = [];
     $(
-      /* Pagetree and table-chart macros */
+      /* List of unsupported macros */
       `[data-macro-name="pagetree"],
-      [data-macro-name="table-chart"]`
-    )
-    .filter(() => {
-      const macroName = $(this).data('macro-name') ?? '';
-      if (macrosFound.indexOf(macroName) !== -1) return false;
-      macrosFound.push(macroName);
-      return true;
-    }) // remove duplicates
-    .each((_: number, element: cheerio.Element) => {
-        const thisBlock = $(element).html();
-        if (!thisBlock) return;
-
-        const macroName = $(element).data('macro-name') ?? 'unnamed macro';
-        $(element).replaceWith('');
-
-        if (context.getView() === 'debug') {          
-          $('#Content h1').after(`
-          <div class="unsupported-macro-indicator">
-            Sorry, unfortunately <b>${macroName}</b> is not supported by Konviw
-            <span class="cross" onclick="(() => {this.parentNode.classList.add('hidden')})()">x</span>
-          </div>`);        
-        }
+      [data-macro-name="table-chart"],
+      [data-macro-name="pagetreesearch"]`,
+    ).each((_: number, element: cheerio.Element) => {
+      const thisBlock = $(element).html();
+      if (!thisBlock) return;
+      const macroName = $(element).data('macro-name') ?? 'unnamed macro';
+      if (context.getView() === 'debug') {
+        // not yet working the display of the macroName, while it works in console.log not in html
+        // console.log('unsupported', macroName);
+        $(element).wrap(`<div class="unsupported-macro-indicator">
+          😞 Sorry, unfortunately ${macroName} is not supported by Konviw
+          <span class="cross" onclick="(() => {this.parentNode.classList.add('hidden')})()">
+          `);
+      }
+      $(element).remove();
     });
     context.getPerfMeasure('addUnsupportedMacroIndicator');
   };
