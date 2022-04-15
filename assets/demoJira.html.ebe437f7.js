4b4f696fb3dcf521a5f2866f4d937b5e63abef33
kiru42 <kiru42@users.noreply.github.com>
2022-04-15 10:17:09 +0000

    deploy: 7c81ad64416b8e1d1264e53ddad025afc62532cd

---
+++ assets/demoJira.html.ebe437f7.js
@@ -0,0 +1,1 @@
+const e={key:"v-f064de22",path:"/demoJira.html",title:"Demo Jira Macro",lang:"en-US",frontmatter:{title:"Demo Jira Macro"},excerpt:"",headers:[],git:{updatedTime:1650017783e3,contributors:[{name:"Kiruban PREMKUMAR",email:"36277587+kiru42@users.noreply.github.com",commits:1}]},filePathRelative:"demoJira.md"};export{e as data};
