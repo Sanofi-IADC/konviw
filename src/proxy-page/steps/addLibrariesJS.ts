0de3dbb071fe818a5d045dc4bc3ad66fb8b01f90
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-04-20 15:06:59 +0200

    fix: using the AntiOverflowFontResizer on table headers to adapt the font in case there is not enough room for the text

--- src/proxy-page/steps/addLibrariesJS.ts
+++ src/proxy-page/steps/addLibrariesJS.ts
@@ -53,6 +53,16 @@ export default (): Step => {
       </script>`,
     );
 
+    $('body').append(
+      `<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>`,
+      `<script src="https://moka.tv/labs/antioverflowfontresizer/AntiOverflowFontResizer.min.js"></script>`,
+      `<script>
+         document.addEventListener('DOMContentLoaded', (event) => {
+           $('th.confluenceTh').aofsr();
+         })
+       </script>`,
+    );
+
     context.getPerfMeasure('addlibrariesJS');
   };
 };
