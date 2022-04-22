5fe64a1c170ed4f4b7887b634c80fc8768b44140
Zougouda <Zougouda@users.noreply.github.com>
2022-04-22 14:31:54 +0000

    deploy: 971bff6e08e35743e335359e7dbce6ab11412a84

---
+++ assets/demoSlidesDocs.html.ad8511fe.js
@@ -0,0 +1,1 @@
+const e={key:"v-e99a0e80",path:"/demoSlidesDocs.html",title:"How to create Konviw Slides",lang:"en-US",frontmatter:{title:"How to create Konviw Slides"},excerpt:"",headers:[],git:{updatedTime:1650637868e3,contributors:[{name:"Cyril Annette",email:"zougouda@gmail.com",commits:1}]},filePathRelative:"demoSlidesDocs.md"};export{e as data};
