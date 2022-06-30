787fc7a8861ed8bd27a32c1907df041120f481c8
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-06-30 13:51:15 +0200

    fix: WEB-178 without different resolution

--- tests/e2e/cypress/integration/tables.spec.ts
+++ tests/e2e/cypress/integration/tables.spec.ts
@@ -3,7 +3,6 @@
 context('Tables', () => {
   it('match the whole page', () => {
     cy.visit('/wiki/spaces/KONVIW/pages/77627467/Demo+Tables').then(() => {
-      cy.viewport(1280, 768)
       cy.document().toMatchImageSnapshot();
     });
   });
