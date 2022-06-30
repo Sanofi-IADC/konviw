50bbd41fe2e39c4c3465abf98681cfe5975a80c5
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-06-30 13:24:17 +0200

    fix: WEB-178 removing 'if' statement about screenshot uploads and putting back the table test

--- tests/e2e/cypress/integration/tables.spec.ts
+++ tests/e2e/cypress/integration/tables.spec.ts
@@ -3,8 +3,8 @@
 context('Tables', () => {
   it('match the whole page', () => {
     cy.visit('/wiki/spaces/KONVIW/pages/77627467/Demo+Tables').then(() => {
-      // cy.viewport(1280, 768)
-      // cy.document().toMatchImageSnapshot();
+      cy.viewport(1280, 768)
+      cy.document().toMatchImageSnapshot();
     });
   });
 });
