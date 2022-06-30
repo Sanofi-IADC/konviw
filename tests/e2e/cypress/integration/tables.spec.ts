02a415374a1768dfe6ad6f275029e325ea721ac0
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-06-30 15:23:32 +0200

    fix: WEB-178 the return of the table cypress test with a new snapshot

--- tests/e2e/cypress/integration/tables.spec.ts
+++ tests/e2e/cypress/integration/tables.spec.ts
@@ -3,8 +3,7 @@
 context('Tables', () => {
   it('match the whole page', () => {
     cy.visit('/wiki/spaces/KONVIW/pages/77627467/Demo+Tables').then(() => {
-      // cy.viewport(1280, 768)
-      // cy.document().toMatchImageSnapshot();
+      cy.document().toMatchImageSnapshot();
     });
   });
 });
