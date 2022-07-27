4ec39232960d4bcd002fc31393b40b79b7623e4a
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-07-27 16:42:13 +0200

    feat: WEB-218 image header has priority over the first image in page-properties when reading a blog page

--- src/proxy-page/steps/addHeaderBlog.ts
+++ src/proxy-page/steps/addHeaderBlog.ts
@@ -7,43 +7,55 @@ export default (): Step => {
     context.setPerfMark('addHeaderBlog');
     const $ = context.getCheerioBody();
 
+    let blogImgSrc = context.getHeaderImage(); // default blog header is the headerImage
+    let blogExcerptString = '', blogHeaderHTML = '', blogExcerptHTML = '';
+
     // Div with class plugin-tabmeta-details is used for macro Page-Properties
     $(".plugin-tabmeta-details[data-macro-name='details']")
       // We just look for the first Page-Properties macro
       .first()
       .each((_index: number, pageProperties: cheerio.Element) => {
-        const thisBlock = $(pageProperties);
         const imgBlog = $(pageProperties).find('img');
-        const excerptBlog = $(pageProperties).find('blockquote');
-        // and take the image and quote to display in the header of the blog post
-        if (thisBlock) {
-          $('#Content').before(
-            `<div class="blog--header"
-               style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-               url('${imgBlog.attr('src')}');">
-           <div class="blog--box">
-             <div class="blog--title">
-               ${context.getTitle()}
-             </div>
-             <div>
-               <img class="userLogo logo" src="${context.getAvatar()}">
-               <div class="blog--vCard">
-                 <div class="blog--when">${context.getFriendlyWhen()} • ${context.getReadTime()} min read</div>
-                 <div>${context.getAuthor()}</div>
-                 <a href="mailto:${context.getEmail()}" class="blog--email">${context.getEmail()}</a>
-               </div>
-             </div>
-           </div>
-         </div>
-         <section class="blog--excerpt">
-           <blockquote>
-             ${excerptBlog.html()}
-           </blockquote>
-         </section>`,
-          );
+        if (!blogImgSrc) {
+          blogImgSrc = imgBlog?.attr('src'); // headerIMage has priority over page-proterties's image
         }
+        blogExcerptString = $(pageProperties).find('blockquote')?.html();
         $(pageProperties).remove();
       });
+
+    if (blogImgSrc) {
+      blogHeaderHTML = `
+        <div class="blog--header"
+          style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+          url('${blogImgSrc}');">
+          <div class="blog--box">
+            <div class="blog--title">
+              ${context.getTitle()}
+            </div>
+            <div>
+              <img class="userLogo logo" src="${context.getAvatar()}">
+              <div class="blog--vCard">
+                <div class="blog--when">${context.getFriendlyWhen()} • ${context.getReadTime()} min read</div>
+                <div>${context.getAuthor()}</div>
+                <a href="mailto:${context.getEmail()}" class="blog--email">${context.getEmail()}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+    }
+    if (blogExcerptString) {
+      blogExcerptHTML = `
+        <section class="blog--excerpt">
+          <blockquote>${blogExcerptString}</blockquote>
+        </section>
+        `;
+    }
+
+    $('#Content').before(`
+        ${blogHeaderHTML}
+        ${blogExcerptHTML}
+      `);
 
     context.getPerfMeasure('addHeaderBlog');
   };
