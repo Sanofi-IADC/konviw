020b44442ddcab72a23e5ff7b44073fde99315b4
PiiXiieeS <PiiXiieeS@users.noreply.github.com>
2022-08-11 12:16:50 +0000

    deploy: 7f63df553b8637721fd82181fce6c3998cdd270e

--- assets/KonviwVersion.9620a740.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./app.99d73187.js";import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";const e="2.19.8",s={name:"KonviwVersion",data(){return{konviwVersion:e}}};function t(i,a,c,p,o,_){return r(o.konviwVersion)}var v=n(s,[["render",t]]);export{v as default};
