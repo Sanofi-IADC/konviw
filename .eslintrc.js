44dfc3535f7c16211f9619ae4f272fa2014cd9bc
Cyril ANNETTE <Cyril.Annette-ext@sanofi.com>
2022-08-22 14:43:20 +0200

    fix: WEB-290 updated prettierc and eslintrc files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,77 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    es2021: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        'singleQuote': true,
-        'semi': true,
-        'trailingComma': 'all'
+
+  parserOptions: { sourceType: 'module' },
+
+  overrides: [
+    {
+      files: ['.eslintrc.js'],
+      extends: ['plugin:prettier/recommended'],
+      plugins: ['prettier'],
+    },
+
+    {
+      files: ['*.ts'],
+
+      parser: '@typescript-eslint/parser',
+
+      extends: [
+        'airbnb-base',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'],
       },
-    ],
-  },
+
+      plugins: ['@typescript-eslint/eslint-plugin', 'import', 'prettier'],
+
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.ts'],
+            paths: ['src'],
+          },
+        },
+      },
+      rules: {
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            ts: 'never',
+          },
+        ],
+
+        camelcase: 'off', // idac habits
+        'implicit-arrow-linebreak': 'off', // idac habits
+        'import/extensions': 'off', // idac habits
+        'import/no-extraneous-dependencies': ['error'], // idac habits
+        'import/no-cycle': 'off', // idac habits
+        'import/prefer-default-export': 'off', // idac habits
+        'max-len': ['warn', { code: 140 }],
+        'no-shadow': 'off', // idac habits
+        'no-underscore-dangle': 'off', // necessary for graphql convention
+        'no-use-before-define': 'off', // idac habits
+        'no-useless-constructor': 'off', // idac habits
+        '@typescript-eslint/no-useless-constructor': 'error', // idac habits
+        '@typescript-eslint/no-shadow': ['error'], // idac habits
+        '@typescript-eslint/no-explicit-any': 'off', // idac habits
+        '@typescript-eslint/interface-name-prefix': 'off', // idac habits
+        '@typescript-eslint/explicit-function-return-type': 'off', // idac habits
+      },
+    },
+    {
+      files: ['**/__tests__/*.{t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
