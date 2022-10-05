a5728ded8fb28f550383002df6916ca25e052077
krystiankedra <kedrakrystian@gmail.com>
2022-10-05 09:55:35 +0200

    fix: WEB-353 Changed logic for recalculation maxHeight of expanded content when includes images

--- src/proxy-page/steps/fixExpander.ts
+++ src/proxy-page/steps/fixExpander.ts
@@ -8,20 +8,39 @@ export default (): Step => (context: ContextService): void => {
   // When the DOM content is loaded call the initialization of the Zooming library
   $('body').append(
     `<script type="module">
-        const coll = document.getElementsByClassName("expand-control");
-            let i;
-            for (i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.maxHeight){
-                  content.style.maxHeight = null;
-                } else {
-                  content.style.maxHeight = content.scrollHeight + "px";
-                } 
+      const coll = document.getElementsByClassName("expand-control");
+      let i;
+      for (i = 0; i < coll.length; i++) {
+        let promiseFinallyState = false;
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          const content = this.nextElementSibling;
+          const toggleMaxHeight = (clear = false) =>
+            content.style.maxHeight = clear ? null : content.scrollHeight + "px";
+          const toggleOpacity = (value) => content.style.opacity = value;
+          const togglePromiseFinallyState = () => promiseFinallyState = !promiseFinallyState;
+          if (content.style.maxHeight) {
+            toggleMaxHeight(true);
+          } else {
+            const imagesCollection = content.querySelectorAll('img');
+            if (imagesCollection && imagesCollection.length > 0 && !promiseFinallyState) {
+              toggleOpacity('0');
+              toggleMaxHeight();
+              const loadImageStatusFn = (image) => new Promise(
+                (res) => image.addEventListener('load', () => res())
+              );
+              Promise.all([...imagesCollection].map(loadImageStatusFn)).finally(() => {
+                toggleMaxHeight();
+                toggleOpacity('1');
+                togglePromiseFinallyState();
               });
+            } else {
+              toggleMaxHeight();
             }
-      </script>`,
+          }
+        });
+      }
+    </script>`,
   );
 
   context.getPerfMeasure('fixExpander');
