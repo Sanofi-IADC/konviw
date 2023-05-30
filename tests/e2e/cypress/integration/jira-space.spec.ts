f8cd9f9bc42d7dd780b40b00ada8479d911336c6
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2023-05-30 10:17:48 +0200

    feat: WEB-1927 Improved cypress test for jira space

--- tests/e2e/cypress/integration/jira-space.spec.ts
+++ tests/e2e/cypress/integration/jira-space.spec.ts
@@ -1,12 +1,16 @@
 /// <reference types="cypress" />
 
-context('Jira spaces', () => {
+context('Jira space', () => {
   it('check jira space link and match the whole page', () => {
     cy.visit(
       '/wiki/spaces/KONVIW/pages/237142017/Demo+Jira+Space',
     ).then(() => {
       cy.wait(2000);
-      cy.document().toMatchImageSnapshot();
+      cy.get('a')
+        .then((anchor) => expect(anchor.text().trim() === 'Konviw').to.be.true)
+        .then(() => {
+          cy.document().toMatchImageSnapshot();
+        });
     });
   });
 });
