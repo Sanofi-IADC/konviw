0889f10b4c42a1d4244e343dc5f67c1610095f15
Krishna Das <krishna.das-ext@sanofi.com>
2023-05-31 14:10:40 +0530

    feat: ARM-1296 Create API for Jira Issue Page

--- src/jira/jira.service.ts
+++ src/jira/jira.service.ts
@@ -66,12 +66,15 @@ export class JiraService {
    * @description Return a the tickets selected in the Jira macro
    * @return Promise {any}
    * @param jqlSearch {string} 'project = FND ORDER BY resolution DESC' - Jira Query Language to filter the issues to retrieve
+   * @param fields {string} 'fields=field1,field2&fields=field3' - A list of fields to return for each issue
    * @param maxResult {number} 100 - maximum number of issues retrieved
+   * @param startAt {number} 15 - starting position to handle paginated results
    */
-  findTickets(
+  async findTickets(
     server: string,
     jqlSearch: string,
     fields: string,
+    startAt = 0,
     maxResult = 100,
   ): Promise<any> {
     // Load new base URL and credencials if defined a specific connection for Jira as ENV variables
@@ -84,11 +87,11 @@ export class JiraService {
     }
     return firstValueFrom(
       this.http.get(
-        `${this.baseUrl}/rest/api/2/search?jql=${jqlSearch}&fields=${fields}&maxResults=${maxResult}`,
+        `${this.baseUrl}/rest/api/3/search?jql=${jqlSearch}&fields=${fields}&maxResults=${maxResult}&startAt=${startAt}`,
         { auth: { username: this.apiUsername, password: this.apiToken } },
       ),
     )
-      .then((res) => res.data)
+      .then((response) => response.data)
       .catch((e) => {
         this.logger.log(e, 'error:findTickets');
       });
@@ -98,7 +101,7 @@ export class JiraService {
    * @function findProjects Service
    * @description Return a the projects matching the filter criteria
    * @return Promise {any}
-   * @param server {string} 'project = FND ORDER BY resolution DESC' - Jira Query Language to filter the issues to retrieve
+   * @param server {string} 'Syatem Jira' - Jira server to search in
    * @param search {string} 'iadc - word to be searched
    * @param startAt {number} 15 - starting position to handle paginated results
    */
@@ -178,5 +181,39 @@ export class JiraService {
         404,
       );
     }
+  }
+
+  /**
+   * @function findProjectIssueTypesWithStatus Service
+   * @description Returns the valid statuses for a project. The statuses are grouped by issue type,
+   *              as each project has a set of valid issue types and each issue type has a set of valid statuses.
+   * @return Promise {any}
+   */
+  async findProjectIssueTypesWithStatus(server: string, projectKey: string): Promise<AxiosResponse> {
+    // Load new base URL and credencials if defined a specific connection for Jira as ENV variables
+    const key = `CPV_JIRA_${server.replace(/\s/, '_')}`;
+    const baseUrl = process.env[`${key}_BASE_URL`];
+    if (baseUrl) {
+      this.baseUrl = baseUrl;
+      this.apiUsername = process.env[`${key}_API_USERNAME`];
+      this.apiToken = process.env[`${key}_API_TOKEN`];
+    }
+    return firstValueFrom(
+      this.http.get(
+        `${this.baseUrl}/rest/api/3/project/${projectKey}/statuses`,
+        { auth: { username: this.apiUsername, password: this.apiToken } },
+      ),
+    )
+      .then((response) => {
+        this.logger.log(`Retrieving project statuses from ${server} for project - ${projectKey}`);
+        return response;
+      })
+      .catch((err) => {
+        this.logger.log(err, 'error:findProjectIssuesTypeWithStatus');
+        throw new HttpException(
+          `error:API findProjectIssuesTypeWithStatus for server ${server} > ${err}`,
+          404,
+        );
+      });
   }
 }
