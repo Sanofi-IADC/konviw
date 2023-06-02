3c78693180b4b678aa630ef9e2efb2081f76b05f
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2023-06-02 09:20:32 +0200

    feat: WEB-1912 Removed matomo

--- src/proxy-page/steps/addWebStatsTracker.ts
+++ src/proxy-page/steps/addWebStatsTracker.ts
@@ -3,11 +3,10 @@ import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 
 /**
- * ### Proxy page step to add Web statistics trackers for Matomo or Google.
+ * ### Proxy page step to add Web statistics trackers for Google.
  *
- * This module gets Cheerio to append to the head of the page the web tracker for Matomo or Google Analytics. Env variables must be defined:
+ * This module gets Cheerio to append to the head of the page the web tracker for Google Analytics. Env variables must be defined:
  *
- * - `matomoBaseURL` and `matomoIdSite` for Matomo
  * - `googleTag` for Google Analytics
  *
  * @param  {ConfigService} config
@@ -16,29 +15,7 @@ import { Step } from '../proxy-page.step';
 export default (config: ConfigService): Step => (context: ContextService): void => {
   context.setPerfMark('addWebStatsTracker');
   const $ = context.getCheerioBody();
-  const matomoBaseURL = config.get('matomo.baseURL');
-  const matomoIdSite = config.get('matomo.idSite');
   const googleTag = config.get('google.tag');
-
-  if (matomoBaseURL && matomoIdSite) {
-    $('head').append(
-      `<!-- Matomo -->
-      <script type="text/javascript">
-        var _paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="${matomoBaseURL}/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '${matomoIdSite}']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-      </script>
-      <!-- End Matomo Tracker -->`,
-    );
-  }
 
   if (googleTag) {
     $('head').append(
