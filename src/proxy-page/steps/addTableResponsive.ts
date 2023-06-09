185c7ac10955947f6a7b4419021aa15510592628
Hoang <Benjamin.Hoang2@sanofi.com>
2023-06-09 15:06:30 +0200

    add code comments and modify variable convention

--- src/proxy-page/steps/addTableResponsive.ts
+++ src/proxy-page/steps/addTableResponsive.ts
@@ -5,43 +5,51 @@ import { Step } from '../proxy-page.step';
 export default (): Step => (context: ContextService): void => {
   context.setPerfMark('addTableResponsive');
   const $ = context.getCheerioBody();
+  // for each table
   $('tbody').each((_index: number, tbodyElement: cheerio.Element) => {
     // Handling headers
-    const headers: string[] = [];
-    const headers_lign: string[] = [];
+    const headers_column: string[] = [];
+    const headers_row: string[] = [];
     const tbody = $(tbodyElement);
+    // for each row in the table
     const trElements = tbody.find('tr');
-    trElements.each((_index1: number, trElement: cheerio.Element) => {
+    trElements.each((_: number, trElement: cheerio.Element) => {
       const tr = $(trElement);
       const thElement = tr.find('th');
       const tdElement = tr.find('td');
+      // row headers and column headers have 2 differents patterns, the column headers are in the first
+      // tr full of th for so there is no td in the first tr
       if (tdElement.length === 0) {
-        thElement.each((_index2: number, thElements: cheerio.Element) => {
+        thElement.each((_: number, thElements: cheerio.Element) => {
           const thElement_text = $(thElements).text();
-          headers.push(thElement_text);
+          headers_column.push(thElement_text);
         });
+      // for row headers they are situated in the first children of each tr 
       } else {
         const thElement_text = thElement.text();
-        if (thElement_text.length !== 0) {
-          headers_lign.push(thElement_text);
+        if (thElement_text) {
+          headers_row.push(thElement_text);
         }
       }
     });
-    const slice_number = headers.length !== 0 && headers_lign.length !== 0 ? 1 : 0;
+    // case where we have column and row headers we want the column header array to start at 1 for td
+    // so if headers_column and headers_row are not both empty
+    const slice_number = headers_column.length > 0 && headers_row.length > 0 ? 1 : 0;
 
-    trElements.slice(slice_number).each((_index3: number, trElement: cheerio.Element) => {
-      const ligne = _index3;
+    trElements.slice(slice_number).each((_index_row: number, trElement: cheerio.Element) => {
+      const row = _index_row;
       const tr = $(trElement);
       const tdElements = tr.find('td');
-      tdElements.each((_index4: number, tdElement: cheerio.Element) => {
+      tdElements.each((_index_column: number, tdElement: cheerio.Element) => {
+        // add header_column and header_row content to each td
         const td = $(tdElement);
-        const header = headers[_index4 + slice_number];
-        const header_lign = headers_lign[ligne];
+        const header_column = headers_column[_index_column + slice_number];
+        const header_row = headers_row[row];
         if (!td.attr('data-column-id')) {
-          td.attr('data-column-id', header);
+          td.attr('data-column-id', header_column);
         }
         if (!td.attr('data-lign-id')) {
-          td.attr('data-lign-id', header_lign);
+          td.attr('data-lign-id', header_row);
         }
       });
     });
