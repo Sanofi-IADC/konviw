6f64ba1de2ab69693d8dba8651f5bb5293ad127e
Hoang <Benjamin.Hoang2@sanofi.com>
2023-06-13 10:02:19 +0200

    Add creator name and page version

--- src/proxy-page/steps/addAuthor_Version.ts
+++ src/proxy-page/steps/addAuthor_Version.ts
@@ -7,10 +7,9 @@ export default (): Step => (context: ContextService): void => {
   const $ = context.getCheerioBody();
   const author = context.getAuthor();
   const image_author = context.getAvatar();
-  const version = context.getLastVersion()
-  
+  const version = context.getLastVersion();
   const firstH1 = $('h1:first-of-type');
-  firstH1.after(`<div class="author_header"><img src="${image_author}" class="author_image"><div class="author_textbox"><p class="author_text">Creator : ${author}</p><p class="author_text">Page version : ${version && version.versionNumber}</p></div></div>`);
+  firstH1.after(`<div class="author_header"><img src="${image_author}" class="author_image"><div class="author_textbox">
+  <p class="author_text">Creator : ${author}</p><p class="author_text">Page version : ${version && version.versionNumber}</p></div></div>`);
   context.getPerfMeasure('addAuthor_Version');
-  
 };
--- tests/unit/steps/addAuthor_Version.spec.ts
+++ tests/unit/steps/addAuthor_Version.spec.ts
@@ -1,10 +1,10 @@
-import { ConfigService } from '@nestjs/config';
-import { ContextService } from '../../../src/context/context.service';
-import { Step } from '../../../src/proxy-page/proxy-page.step';
-import addAuthor_Version from '../../../src/proxy-page/steps/addAuthor_Version';
-import { createModuleRefForStep } from './utils';
-import {Version} from '../../../src/context/context.interface';
-describe('Confluence Proxy / addTheme', () => {
+import { ConfigService } from "@nestjs/config";
+import { ContextService } from "../../../src/context/context.service";
+import { Step } from "../../../src/proxy-page/proxy-page.step";
+import addAuthor_Version from "../../../src/proxy-page/steps/addAuthor_Version";
+import { createModuleRefForStep } from "./utils";
+import { Version } from "../../../src/context/context.interface";
+describe("Confluence Proxy / addTheme", () => {
   let context: ContextService;
   let step: Step;
 
@@ -15,21 +15,26 @@ describe('Confluence Proxy / addTheme', () => {
     context.setAuthor("Test");
     context.setAvatar("Test.jpg");
     const mockversion: Version = {
-        versionNumber: 2,
-        when: "Test",
-        modificationBy: {
-          displayName: "Test",
-          email: "Test@sanofi.com",
-          profilePicture: `Test.jpg`,
-        },
-      };
-    context.setLastVersion(mockversion)
+      versionNumber: 2,
+      when: "Test",
+      modificationBy: {
+        displayName: "Test",
+        email: "Test@sanofi.com",
+        profilePicture: `Test.jpg`,
+      },
+    };
+    context.setLastVersion(mockversion);
   });
 
-  it('should add data-column-id from th to each td (column headers)', () => {
-    context.setHtmlBody('<html><head></head><body><div id="Content"><h1 class="titlePage"> Demo table</h1>'+
-    '</body></html>');
+  it("should add data-column-id from th to each td (column headers)", () => {
+    context.setHtmlBody(
+      '<html><head></head><body><div id="Content"><h1 class="titlePage"> Demo table</h1>' +
+        "</body></html>"
+    );
     step(context);
-    expect(context.getHtmlBody()).toContain('<div class="author_header"><img src="Test.jpg" class="author_image"><div class="author_textbox"><p class="author_text">Creator : Test</p><p class="author_text">Page version : 2</p></div></div>');
+    expect(context.getHtmlBody()).toMatchInlineSnapshot(`
+      "<html><head></head><body><div id=\\"Content\\"><div id=\\"Content\\"><h1 class=\\"titlePage\\"> Demo table</h1><div class=\\"author_header\\"><img src=\\"Test.jpg\\" class=\\"author_image\\"><div class=\\"author_textbox\\">
+        <p class=\\"author_text\\">Creator : Test</p><p class=\\"author_text\\">Page version : 2</p></div></div></div></div></body></html>"
+    `);
   });
 });
