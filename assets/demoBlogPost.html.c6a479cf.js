e06249d16d9eeba23f8bdbee4c29afa778c622f8
krystiankedra <krystiankedra@users.noreply.github.com>
2023-06-13 09:16:02 +0000

    deploy: afc3241d479e02ea37f86450620ac386df4e5e16

--- assets/demoBlogPost.html.c6a479cf.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-6472688a",path:"/demoBlogPost.html",title:"Demo with Blog Post",lang:"en-US",frontmatter:{title:"Demo with Blog Post"},excerpt:"",headers:[],git:{updatedTime:1686033089e3,contributors:[{name:"semantic-release-bot",email:"semantic-release-bot@martynus.net",commits:1}]},filePathRelative:"demoBlogPost.md"};export{t as data};
