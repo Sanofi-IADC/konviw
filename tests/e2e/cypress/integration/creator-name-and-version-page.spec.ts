84b5dd52e2925602998ad0722b38137c91141bc1
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2023-06-14 08:44:46 +0200

    feat: WEB-270 Fixed indent

--- tests/e2e/cypress/integration/creator-name-and-version-page.spec.ts
+++ tests/e2e/cypress/integration/creator-name-and-version-page.spec.ts
@@ -1,12 +1,11 @@
 /// <reference types="cypress" />
 
 context('Check if the header is displayed', () => {
-    it('the header should display correctly', () => {
-      cy.visit(
-        '/wiki/spaces/KONVIW/pages/238354433/Random+Confluence+Page',
-      ).then(() => {
-        cy.document().toMatchImageSnapshot();
-      });
+  it('the header should display correctly', () => {
+    cy.visit(
+      '/wiki/spaces/KONVIW/pages/238354433/Random+Confluence+Page',
+    ).then(() => {
+      cy.document().toMatchImageSnapshot();
     });
   });
-  
+});
