91a15a5dd40ac713e9b41345ba16cb9d8fe51808
krystiankedra <krystiankedra@users.noreply.github.com>
2024-01-15 11:44:11 +0000

    deploy: 3ff02ad4006a9ed8e3a14c4001ac241e45dffdf2

---
+++ assets/demoJira.html.5203a85f.js
@@ -0,0 +1,1 @@
+const e={key:"v-f064de22",path:"/demoJira.html",title:"Demo Jira Macro",lang:"en-US",frontmatter:{title:"Demo Jira Macro"},excerpt:"",headers:[],git:{updatedTime:1705319023e3,contributors:[{name:"Krystian K\u0119dra",email:"kedrakrystian@gmail.com",commits:1}]},filePathRelative:"demoJira.md"};export{e as data};
