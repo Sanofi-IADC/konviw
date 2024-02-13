9af2ba66081c7ca8def471d4fcc36ba5b9f3fab7
Krystian Kedra <krystian.kedra-ext@sanofi.com>
2024-02-13 22:03:35 +0100

    feat: WEB-2203 Fixed wrong name

--- tests/e2e/cypress/integration/jira-issues.spec.ts
+++ tests/e2e/cypress/integration/jira-issues.spec.ts
@@ -1,17 +1,16 @@
 /// <reference types="cypress" />
 
 context('Jira issues', () => {
-    it('check jira space link and match the whole page', () => {
-      cy.visit(
-        '/wiki/spaces/KONVIW/pages/345931777/Demo+Jira+Issues',
-      ).then(() => {
-        cy.wait(2000);
-        cy.get('grid,gridjs-container')
-          .then((container) => expect(Boolean(container)).to.be.true)
-          .then(() => {
-            cy.document().toMatchImageSnapshot();
-          });
-      });
+  it('should render grid container for new jira issues macro and match the whole page', () => {
+    cy.visit(
+      '/wiki/spaces/KONVIW/pages/345931777/Demo+Jira+Issues',
+    ).then(() => {
+      cy.wait(2000);
+      cy.get('grid,gridjs-container')
+        .then((container) => expect(Boolean(container)).to.be.true)
+        .then(() => {
+          cy.document().toMatchImageSnapshot();
+        });
     });
   });
-  
+});
