79e4510efc2a0e936eccb9f6e104fa7400457b39
PiiXiieeS <PiiXiieeS@users.noreply.github.com>
2024-04-27 18:45:13 +0000

    deploy: e590f38492904e6f71fd3a9bb169293045d40f59

---
+++ assets/demoBlogPost.html.53ef68d6.js
@@ -0,0 +1,1 @@
+const t={key:"v-6472688a",path:"/demoBlogPost.html",title:"Demo with Blog Post",lang:"en-US",frontmatter:{title:"Demo with Blog Post"},excerpt:"",headers:[],git:{updatedTime:1714243478e3,contributors:[{name:"Jose Gascon",email:"PiiXiieeS@users.noreply.github.com",commits:1}]},filePathRelative:"demoBlogPost.md"};export{t as data};
