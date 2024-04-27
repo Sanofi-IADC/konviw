12dcb2e32249edc4cc37ce60fd538f335b162312
PiiXiieeS <piixiiees@gmail.com>
2024-04-27 17:52:07 +0200

    fix: display new feature inline icon
    - fixImageSize now fixes inline icons
    - disable the fixTableSize as it is not needed anymore
    
    resolves WEB-2291

--- src/proxy-page/steps/fixImageSize.ts
+++ src/proxy-page/steps/fixImageSize.ts
@@ -1,4 +1,5 @@
 import * as cheerio from 'cheerio';
+import { Logger } from '@nestjs/common';
 import { ContextService } from '../../context/context.service';
 import { Step } from '../proxy-page.step';
 
@@ -6,17 +7,19 @@ import { Step } from '../proxy-page.step';
  * ### Proxy page step to fix image width
  *
  * This module gets Cheerio to search all images ('img')
- * and set the width according to attribute 'data-width'
+ * with class 'confluence-embedded-image' and
  * if the width attr was not directly provided by the API
+ * sets the width to display as mini-icon
  *
  * @param  {ConfigService} config
  * @returns void
  */
 export default (): Step => (context: ContextService): void => {
   context.setPerfMark('fixImageSize');
+  const logger = new Logger('fixImageSize');
   const $ = context.getCheerioBody();
 
-  $('img').each((_index: number, elementImg: cheerio.Element) => {
+  $('img.confluence-embedded-image').each((_index: number, elementImg: cheerio.Element) => {
     const imgURL = $(elementImg).attr('src') || $(elementImg).attr('_src');
     const imgDataWidth = $(elementImg).attr('data-width');
     if (
@@ -24,7 +27,8 @@ export default (): Step => (context: ContextService): void => {
         && imgDataWidth != null
         && !$(elementImg).attr('width')
     ) {
-      $(elementImg).attr('width', imgDataWidth);
+      $(elementImg).attr('width', '27px');
+      logger.log('Fixed width to inline icon');
     }
   });
 
