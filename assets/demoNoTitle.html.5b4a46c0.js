07f89f48fe67861cb66bad12e9b06e8a19fd2fb0
krystiankedra <krystiankedra@users.noreply.github.com>
2024-08-14 10:55:22 +0000

    deploy: e705f48e0a078ca649576dfd642ece42f262ee8c

--- assets/demoNoTitle.html.5b4a46c0.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-46bab89e",path:"/demoNoTitle.html",title:"Demo without Title",lang:"en-US",frontmatter:{title:"Demo without Title"},excerpt:"",headers:[],git:{updatedTime:1723621527e3,contributors:[{name:"Krystian K\u0119dra",email:"kedrakrystian@gmail.com",commits:1}]},filePathRelative:"demoNoTitle.md"};export{t as data};
---
+++ assets/demoStyles.html.29b0be42.js
@@ -0,0 +1,1 @@
+const e={key:"v-76caaf3b",path:"/demoStyles.html",title:"Demo Styles",lang:"en-US",frontmatter:{title:"Demo Styles"},excerpt:"",headers:[],git:{updatedTime:172363287e4,contributors:[{name:"Krystian K\u0119dra",email:"kedrakrystian@gmail.com",commits:1}]},filePathRelative:"demoStyles.md"};export{e as data};
