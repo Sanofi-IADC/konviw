7903c396647ab328e983e40a589824cb69508ce6
kr1shnadas <kr1shnadas@users.noreply.github.com>
2025-01-07 17:31:29 +0000

    deploy: 49df5cc30617cca8abf87ef750b8a59cd2420d73

---
+++ assets/demoCharts.html.0f27f5fd.js
@@ -0,0 +1,1 @@
+const a={key:"v-576853a8",path:"/demoCharts.html",title:"Demo Chart Macro",lang:"en-US",frontmatter:{title:"Demo Chart Macro"},excerpt:"",headers:[],git:{updatedTime:1736271058e3,contributors:[{name:"Krishna Das",email:"emailkrishnadas@gmail.com",commits:1}]},filePathRelative:"demoCharts.md"};export{a as data};
