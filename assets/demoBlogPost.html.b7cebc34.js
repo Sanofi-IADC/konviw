f993d720246e27eeb77c257deff826f3096ef6a0
BENBEN5959 <BENBEN5959@users.noreply.github.com>
2025-04-01 11:59:42 +0000

    deploy: f6b146604384beb8c7dbaae8c62102ff3ea09c0f

--- assets/demoBlogPost.html.b7cebc34.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-6472688a",path:"/demoBlogPost.html",title:"Demo with Blog Post",lang:"en-US",frontmatter:{title:"Demo with Blog Post"},excerpt:"",headers:[],git:{updatedTime:1742782535e3,contributors:[{name:"Jose Gascon",email:"PiiXiieeS@users.noreply.github.com",commits:1}]},filePathRelative:"demoBlogPost.md"};export{t as data};
