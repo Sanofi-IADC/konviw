fc499248c54f2627696e55ff0fd83a90ea3737b7
Sairam Charan <mohanvenkat459@gmail.com>
2025-04-07 11:02:02 +0530

    fix: WEB-2498 remove tokens from logs

--- src/jira/jira.service.ts
+++ src/jira/jira.service.ts
@@ -132,9 +132,7 @@ export class JiraService {
       'path',
       `${this.baseUrl}/rest/api/3/search?jql=${encodeURIComponent(jqlSearch)}`,
       'Confluence Username:',
-      this.apiUsername,
-      'token',
-      this.apiToken,
+      this.apiUsername
     );
     if (reader === true) {
       this.init(reader);
