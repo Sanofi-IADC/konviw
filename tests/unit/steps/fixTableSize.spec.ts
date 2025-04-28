ad818851cec706af2585d42bce7cee670de07991
PiiXiieeS <piixiiees@gmail.com>
2025-04-28 08:31:24 +0200

    fix: clean sonarqube warnings WEB-2507

--- tests/unit/steps/fixTableSize.spec.ts
+++ tests/unit/steps/fixTableSize.spec.ts
@@ -23,9 +23,9 @@ describe('Confluence Proxy / fixTableSize', () => {
     '</td></tr><tr><td><p>Test4</p></td><td><p>Test5</p></td><td><p>Test6</p></td></tr>'+
     '</tbody></table></div><p></p></body></html>');
     step(context);
-    expect(context.getHtmlBody()).toEqual('<html><head></head><body><div id=\"Content\"><h1 class=\"titlePage\"> Demo table</h1>'+
-    '<div class=\"table-wrap\">'+
-    '<table data-table-width=\"750\" data-layout=\"default\" class=\"confluenceTable\" style=\"width: 750;\" data-konviw-table-size=\"small\">'+
+    expect(context.getHtmlBody()).toEqual('<html><head></head><body><div id="Content"><h1 class="titlePage"> Demo table</h1>'+
+    '<div class="table-wrap">'+
+    '<table data-table-width="750" data-layout="default" class="confluenceTable" style="width: 750;" data-konviw-table-size="small">'+
     '<colgroup><col><col><col></colgroup>'+
     '<tbody><tr><th><p><strong>A</strong></p></th><th><p><strong>B</strong></p></th><th><p><strong>C</strong></p>'+
     '</th></tr><tr><td><p>Test1</p></td><td><p>Test2</p></td><td><p>Test3</p>'+
@@ -43,9 +43,9 @@ describe('Confluence Proxy / fixTableSize', () => {
     '</td></tr><tr><td><p>Test4</p></td><td><p>Test5</p></td><td><p>Test6</p></td></tr>'+
     '</tbody></table></div><p></p></body></html>');
     step(context);
-    expect(context.getHtmlBody()).toEqual('<html><head></head><body><div id=\"Content\"><h1 class=\"titlePage\"> Demo table</h1>'+
-    '<div class=\"table-wrap\">'+
-    '<table data-table-width=\"1500\" data-layout=\"default\" class=\"confluenceTable\" data-konviw-table-size=\"large\">'+
+    expect(context.getHtmlBody()).toEqual('<html><head></head><body><div id="Content"><h1 class="titlePage"> Demo table</h1>'+
+    '<div class="table-wrap">'+
+    '<table data-table-width="1500" data-layout="default" class="confluenceTable" data-konviw-table-size="large">'+
     '<colgroup><col><col><col></colgroup>'+
     '<tbody><tr><th><p><strong>A</strong></p></th><th><p><strong>B</strong></p></th><th><p><strong>C</strong></p>'+
     '</th></tr><tr><td><p>Test1</p></td><td><p>Test2</p></td><td><p>Test3</p>'+
