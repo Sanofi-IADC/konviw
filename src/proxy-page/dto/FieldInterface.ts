15ae3089e4964e4f29b98577ee9fcd57c14d960c
Sairam Charan <mohanvenkat459@gmail.com>
2025-06-25 16:57:14 +0530

    WEB-2521 remove logs

--- src/proxy-page/dto/FieldInterface.ts
+++ src/proxy-page/dto/FieldInterface.ts
@@ -358,7 +358,6 @@ export const formatString = (string) => {
   if (string?.content) {
     const flatMapResult = string.content.flatMap((item) => item.content);
     const mappedResults = flatMapResult.filter(Boolean).map((subItem) => {
-      console.log(flatMapResult)
       if (subItem.type === 'inlineCard') {
         return subItem.attrs.url;
       }
