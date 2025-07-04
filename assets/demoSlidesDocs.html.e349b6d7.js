6cf1494903452b4cf5ceee5ac48d32467803d209
sairam459 <sairam459@users.noreply.github.com>
2025-07-04 08:26:58 +0000

    deploy: bb557c990f35fe1fac0006a871eb07fe2d9c8a89

---
+++ assets/demoSlidesDocs.html.e349b6d7.js
@@ -0,0 +1,1 @@
+const e={key:"v-e99a0e80",path:"/demoSlidesDocs.html",title:"How to create Konviw Slides",lang:"en-US",frontmatter:{title:"How to create Konviw Slides"},excerpt:"",headers:[],git:{updatedTime:1751617588e3,contributors:[{name:"sairam charan",email:"mohanvenkat459@gmail.com",commits:1}]},filePathRelative:"demoSlidesDocs.md"};export{e as data};
