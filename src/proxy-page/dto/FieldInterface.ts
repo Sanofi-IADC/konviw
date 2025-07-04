71d3bac3e17b2760b3b61fe9e3d853a735b51223
Sairam Charan <mohanvenkat459@gmail.com>
2025-07-04 14:31:55 +0530

    fix: WEB-2521 null check

--- src/proxy-page/dto/FieldInterface.ts
+++ src/proxy-page/dto/FieldInterface.ts
@@ -358,7 +358,7 @@ export const formatString = (string) => {
   if (string?.content) {
     const flatMapResult = string.content.flatMap((item) => item.content);
     const mappedResults = flatMapResult.filter(Boolean).map((subItem) => {
-      if (subItem.type === 'inlineCard') {
+      if (subItem?.type === 'inlineCard') {
         return subItem.attrs.url;
       }
       return getAllValues(subItem, isStringContent, 'text');
