e1044df38922d5bf903525c7aa969cf66229c5e7
Shubham Upadhyay <shubham.upadhyay@sanofi.com>
2025-10-23 15:11:17 +0530

    fix: WEB-2574 fix mjor emoji issue

--- src/proxy-page/steps/addHeaderTitle.ts
+++ src/proxy-page/steps/addHeaderTitle.ts
@@ -14,7 +14,7 @@ export default (confluence: ConfluenceService): Step => async (context: ContextS
     $('#Content').prepend(
       `<h1 class="titlePage">
     <div class="specialAtlassian">
-      ${imgHtml ? `<img src='${imgHtml}' />` : ''}
+      ${imgHtml ? `<img src='${imgHtml}' />` : ''} // no image if path not avaialable or broken 
       &nbsp;${title}
     </div>
   </h1>`,
