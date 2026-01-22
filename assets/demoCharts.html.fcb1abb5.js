dc8333bd9e0f83d77a8dee102d3c98c75fe8c00d
Shubham-Upadhay <Shubham-Upadhay@users.noreply.github.com>
2026-01-22 06:00:07 +0000

    deploy: ca2c00eeb18896bc2d3724a3d7d5cb4f1636df29

--- assets/demoCharts.html.fcb1abb5.js
+++
@@ -1,1 +0,0 @@
-const a={key:"v-576853a8",path:"/demoCharts.html",title:"Demo Chart Macro",lang:"en-US",frontmatter:{title:"Demo Chart Macro"},excerpt:"",headers:[],git:{updatedTime:1769060023e3,contributors:[{name:"Shubham Upadhay",email:"shubham.upadhyay@sanofi.com",commits:1}]},filePathRelative:"demoCharts.md"};export{a as data};
---
+++ assets/demoJira.html.5c28df86.js
@@ -0,0 +1,1 @@
+const a={key:"v-f064de22",path:"/demoJira.html",title:"Demo Jira Macro",lang:"en-US",frontmatter:{title:"Demo Jira Macro"},excerpt:"",headers:[],git:{updatedTime:176906158e4,contributors:[{name:"Shubham Upadhay",email:"shubham.upadhyay@sanofi.com",commits:1}]},filePathRelative:"demoJira.md"};export{a as data};
