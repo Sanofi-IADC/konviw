48e680a506f401bf0b9ae84700770e3eef48a738
shiuli-n <shiuli-n@users.noreply.github.com>
2026-03-13 06:32:46 +0000

    deploy: acf13b8e8c2daf740e1aaa69fc616c319cb1cb5c

---
+++ assets/demoJira.html.a42aa4e6.js
@@ -0,0 +1,1 @@
+const e={key:"v-f064de22",path:"/demoJira.html",title:"Demo Jira Macro",lang:"en-US",frontmatter:{title:"Demo Jira Macro"},excerpt:"",headers:[],git:{updatedTime:1773383536e3,contributors:[{name:"Shiuli Noor Alam",email:"sheikhshiulinoor.alam@sanofi.com",commits:1}]},filePathRelative:"demoJira.md"};export{e as data};
